4493ac396893acaaf3f5aaeb3825d9a4f04a43be
Amro Satti <amrsatti98@gmail.com>
2023-03-08 04:12:23 +0200

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comp3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for fd in range(0, 10):
+    for sd in range(fd + 1, 10):
+        if fd == 8 and sd == 9:
+            print("{}{}".format(fd, sd))
+        else:
+            print("{}{}".format(fd, sd), end=', ')
