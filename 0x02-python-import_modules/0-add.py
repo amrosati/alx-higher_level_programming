0c0e102c8a8aacf8cc7b4311ebee044198f25cd9
Amro Satti <amrsatti98@gmail.com>
2023-03-09 10:17:24 +0200

    fixed execution by "__import__" bug

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-from add_0 import add as addd
 
-a = 1
-b = 2
+def main():
+    from add_0 import add as addd
 
-print("{} + {} = {}".format(a, b, addd(a, b)))
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, addd(a, b)))
+
+
+if __name__ == "__main__":
+    main()
