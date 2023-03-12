69c0168e29ca8ca41fd9bc054827de92d3873fd4
Amro Satti <amrsatti98@gmail.com>
2023-03-12 02:34:26 +0200

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    num = 0
+
+    for x in my_list:
+        if x > num:
+            num = x
+
+    return num
