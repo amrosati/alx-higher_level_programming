6d5e969cbf1d45b051e96c1cca425b0d171e8cec
Amro Satti <amrsatti98@gmail.com>
2023-03-17 00:18:12 +0200

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    res, i = 0, 0
+    romans = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40,
+              'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500,
+              'CM': 900, 'M': 1000}
+
+    while i < len(roman_string):
+        if roman_string[i] not in romans:
+            res = 0
+            break
+        elif i + 2 <= len(roman_string) and roman_string[i:i+1] in romans:
+            res += romans[roman_string[i:i+2]]
+            i += 1
+        else:
+            res += romans[roman_string[i]]
+        i += 1
+
+    return res
