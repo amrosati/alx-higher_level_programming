c3b700881ee1388b92b8e051d66df33183b8b743
Amro Satti <amrsatti98@gmail.com>
2023-03-28 17:42:54 +0200

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for el in my_list:
+        if (n == x):
+            break
+        try:
+            n += 1
+            print("{:d}".format(el), end="")
+        except ValueError:
+            n -= 1
+            continue
+        except IndexError:
+            n -= 1
+            break
+
+    print()
+    try:
+        return n
+    except UnboundLocalError:
+        return 0
