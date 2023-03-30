608e1c6c898f1cfb15251732af7bb45d09906597
Amro Satti <amrsatti98@gmail.com>
2023-03-31 01:29:12 +0200

    fixed my_print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -77,6 +77,8 @@ class Square:
             print()
         else:
             for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print("_", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
