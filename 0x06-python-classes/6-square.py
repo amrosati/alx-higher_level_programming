6c25b43c096757487a8fec9acbe4af448b3659e0
Amro Satti <amrsatti98@gmail.com>
2023-03-31 01:55:38 +0200

    fixed position conditions and my_print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,15 +49,11 @@ class Square:
         Args:
             value (typle): Coordinates of a square
         """
-        error = TypeError("position must be a tuple of 2 positive integers")
-
-        if type(value) is not tuple:
-            raise error
-        if len(value) != 2:
-            raise error
-        for i in value:
-            if i < 0:
-                raise error
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
 
@@ -75,10 +71,10 @@ class Square:
         """prints in stdout the square with the character '#'"""
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
+            return
+
+        [print() for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print()
