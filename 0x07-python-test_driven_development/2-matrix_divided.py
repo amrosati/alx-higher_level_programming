040203da13c907982b0f1045dbf2a743c414ef14
Amro Satti <amrsatti98@gmail.com>
2023-04-06 20:21:43 +0200

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -28,38 +28,25 @@ def matrix_divided(matrix, div):
         TypeError: If the matrix type is not a list,
             its elements also not lists,
             and their elements are not int/float.
-            If the size of rows in matrix aren't equal
+            If the size of rows in matrix aren't equal.
+            If ``div`` is not int/float.
 
         ZeroDivisionError: If ``div`` is 0
     """
-    msg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list:
-        raise TypeError(msg)
+    if (type(matrix) is not list or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all((isinstance(el, int) or isinstance(el, float))
+                for el in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
 
-    new_matrix = []
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-    try:
-        row_size = len(matrix[0])
-    except TypeError:
-        raise TypeError(msg)
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
 
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError(msg)
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-        if len(row) != row_size:
-            raise TypeError("Each row of the matrix must have the same size")
-
-        tmp = []
-        for i in range(row_size):
-            if type(row[i]) is not int and type(row[i]) is not float:
-                raise TypeError(msg)
-
-            if div == 0:
-                raise ZeroDivisionError("division by zero")
-
-            tmp.append(round(row[i] / div, 2))
-
-        new_matrix.append(tmp)
-
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
