efce399069b177482ebd065b6bb4a74bc61f0e06
Amro Satti <amrsatti98@gmail.com>
2023-05-11 09:02:42 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines ``BaseGeometry`` Class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class
+    """
+
+    def area(self):
+        """ToDo
+
+        Raises:
+            Exception
+        """
+        raise Exception("area() is not implemented")
