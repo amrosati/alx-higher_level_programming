ec4f39fa02d7361452c8ad7aad435400fdf82a8f
Amro Satti <amrsatti98@gmail.com>
2023-05-17 11:06:57 +0300

    created project directory and readme file and solved task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines ``read_file()`` function
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout
+
+    Args:
+        filename (str): The filename to read
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.readlines())
