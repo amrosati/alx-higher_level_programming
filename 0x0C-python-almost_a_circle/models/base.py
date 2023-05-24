0a6410b195adabb293657071673bb2c4325bf959
Amro Satti <amrsatti98@gmail.com>
2023-05-24 08:41:54 +0300

    added a static method that encodes objects to json string to the Base class"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 
 """Defines a Base class
 """
+import json
 
 
 class Base:
@@ -20,3 +21,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Encodes a list of dicts object to JSON string
+
+        Args:
+            list_dictionaries (:obj:list): list of dictionaries
+
+        Returns:
+            str: JSON string representation of the list
+        """
+        if list_dictionaries is None:
+            return "[]"
+        
+        return json.dumps(list_dictionaries)
